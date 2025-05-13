c3977d05068975da7fcb8c6edf30c0cc25bbd4fa
PaulElcampeon <pauloladele@live.co.uk>
2025-05-13 21:36:51 +0100

    changed code v1.0.3

--- src/main/java/com/paulo/k8_flux/HelloController.java
+++ src/main/java/com/paulo/k8_flux/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot on Kubernetes! v1.0.2";
+        return "Hello from Spring Boot on Kubernetes! v1.0.3";
     }
 }
